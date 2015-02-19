8c6fb8a8ecbd2a78ca9bbe1b4c0842e2a4553ce8
Dan <danjohnson35@gmail.com>
2015-02-19 01:44:38 -0800

    Removed paragraph tags from JSON response.

--- app/src/main/java/treehousecareerprojects/readie/json/SearchResponseParser.java
+++ app/src/main/java/treehousecareerprojects/readie/json/SearchResponseParser.java
@@ -6,6 +6,8 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import treehousecareerprojects.readie.model.SearchResult;
 
@@ -13,8 +15,11 @@ import treehousecareerprojects.readie.model.SearchResult;
  * Created by Dan on 2/18/2015.
  */
 public final class SearchResponseParser {
+    private static final Pattern snippetPattern = Pattern.compile("<p>(.*)</p>");
+    private static final int SNIPPET_REGEX_ID = 1;
+
     public static List<SearchResult> extractSearchResults(JSONObject json) throws JSONException {
-        List<SearchResult> results = new ArrayList<>();
+        List<SearchResult> results = new ArrayList<SearchResult>();
         JSONArray reviews = json.getJSONArray(SearchResult.REVIEW_ARRAY_JSON_ID);
 
         for(int i = 0; i < reviews.length(); i++) {
@@ -23,12 +28,19 @@ public final class SearchResponseParser {
 
             result.setBookTitle(review.getString(SearchResult.BOOK_TITLE_JSON_ID));
             result.setReviewer(review.getString(SearchResult.REVIEWER_JSON_ID));
-            result.setReviewSnippet(review.getString(SearchResult.REVIEW_SNIPPET_JSON_ID));
+            result.setReviewSnippet(cleanReviewSnippet(review.getString(SearchResult.REVIEW_SNIPPET_JSON_ID)));
             result.setReviewUrl(review.getString(SearchResult.REVIEW_URL_JSON_ID));
 
             results.add(result);
         }
 
         return results;
+    }
+
+    private static String cleanReviewSnippet(String snippet) {
+        Matcher result = snippetPattern.matcher(snippet);
+        result.find();
+
+        return result.group(SNIPPET_REGEX_ID);
     }
 }
